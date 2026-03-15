425c67aade690132d6faad34afa76419c95e0e45
TED_ANUBI <ted.anubi@gmail.com>
2026-03-15 18:19:26 +0300

    Day 1:practice quiz

---
+++ Python/basics/quiz.py
@@ -0,0 +1,28 @@
+# Input
+name = input("Enter your name:" )
+print("Hello,", name)
+
+city = input("Enter your city:")
+print("You are from", city)
+
+# Variables
+name = "ted"
+age = 40
+version = 3.1
+
+print(name)
+print(age)
+print(version)
+print()
+print(type(name))
+print(type(age))
+print(type(version))
+
+# String Method
+full_name = input("Enter full name:")
+print(full_name.upper())
+print(full_name.lower())
+print(full_name.title())
+print(len(full_name))
+
+
