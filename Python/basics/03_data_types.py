e0feb93a6e96d2b1dfb5b9fe028d7d30a49dafe6
TED_ANUBI <ted.anubi@gmail.com>
2026-03-15 17:28:18 +0300

    Day 1:data types

---
+++ Python/basics/03_data_types.py
@@ -0,0 +1,14 @@
+# Data Types
+name = "Ted"
+year = 2026
+gpa = 3.8
+
+print(type(name))
+print(type(year))
+print(type(gpa))
+
+#Checking types directly
+print(type("Mombasa"))
+print(type(99))
+print(type(3.14))
+print(type(True))
