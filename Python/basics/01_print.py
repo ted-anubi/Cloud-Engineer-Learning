467a71e40c67702a821a69884f22de45018ed0b5
TED_ANUBI <ted.anubi@gmail.com>
2026-03-15 17:15:09 +0300

    Day 1:print and variables basics

---
+++ Python/basics/01_print.py
@@ -0,0 +1,6 @@
+# print() function
+print("Hello, World!")
+print("I am training to become a Cloud Security Architect.")
+print(2026)
+print()
+print("Let's go!")
---
+++ Python/basics/02_variables.py
@@ -0,0 +1,16 @@
+# Variables
+name = "Ted"
+city = "Mombasa"
+year = 2026
+
+# print()
+print(name)
+print(city)
+print(year)
+print()
+print("My name is",name)
+print("I am based in",city)
+print("My target year is",year)
+print()
+city = "Nairobi"
+print("I have moved to",city)
