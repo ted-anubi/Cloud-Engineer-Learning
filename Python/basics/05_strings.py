7df45a05c25ef9d24e391cf1af6298fbcacc13ce
TED_ANUBI <ted.anubi@gmail.com>
2026-03-15 17:50:54 +0300

    Day 1:strings methods

---
+++ Python/basics/05_strings.py
@@ -0,0 +1,12 @@
+# String Method
+name = "ted anubi"
+
+print(name.upper())
+print(name.lower())
+print(name.capitalize())
+print(name.title())
+print(len(name))
+
+city = " Mombasa "
+print(city.strip())
+print(city.strip().upper())
