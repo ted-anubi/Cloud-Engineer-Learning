f90ad628e54dc2eb3e129a565b24d640bcbfbd66
TED_ANUBI <ted.anubi@gmail.com>
2026-03-16 13:35:35 +0300

    Day 2: numbers and math

---
+++ Python/basics/05b_strings.py
@@ -0,0 +1,25 @@
+# String Indexing
+name = "TedAnubi"
+print(name[0])   #T
+print(name[1])
+print(name[7])
+print(name[-1])
+
+# String Slicing
+print(name[0:3])
+print(name[3:8])
+print(name[ :3])
+print(name[3: ])
+
+# Replace and find
+message = "I want to be a cloud Engineer"
+print(message.replace("Engineer","Security Architect"))
+print(message.find("cloud"))
+
+# f - strings
+name = "Ted"
+city = "Mombasa"
+year = 2026
+
+print(f"My name is {name} and I am from {city}.")
+print(f"My target certification year is {year}.")
